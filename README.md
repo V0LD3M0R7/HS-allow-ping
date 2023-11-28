cd362103aea3f245ed8e606bd3d6c8b6b89ca06e
Vít Peukert <110905408+V0LD3M0R7@users.noreply.github.com>
2023-11-28 12:37:36 +0100

    Updated readme and script
    MD - description, intendet, credits
    script - y/n dialog and core process

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # HS-allow-ping-deb
 
 ## Fix for HUNT: SHOWDOWN on Debian/Ubuntu
-script to **allow ipv4 ping for Hunt: Showdown**
+Script to **allow ipv4 ping Easy Anti-cheat used in Hunt: Showdown**
+
+### Why does this exist?
+Debian, __/etc/sysctl.d/local.conf__ needs to have this line written:
+
+`net.ipv4.ping_group_range="0 $gid"` where `$gid` is your group id number
+
+This allows EAC to ping and ensure connection between you and EAC servers
+Otherwise Hunt: Showdown will kick you from multiplayer game
 
 ### INTENDET FOR:
 - [x] Debian
 - [x] Ubuntu
-- could potentially help in Debian/Ubuntu flavours
+- should also help in Debian/Ubuntu forked operating systems
 
 #### CREDIST TO ALL SOURCES IN THE sources.txt
+- [KubuntuKowalsky](https://www.youtube.com/@sudorm-doubt)
+- [SamPurple22](https://github.com/SamPurple22)
--- hunt_mp.sh
+++ hunt_mp.sh
@@ -1,6 +1,27 @@
 #! /bin/bash
 
+echo "The script will now enable ipv4 ping for current session" && sleep 2
 sudo sysctl -w net.ipv4.ping_group_range="0 2147483647"
 echo " "
-echo "Completed! Closing in three seconds" && sleep 3
-kill $PPID
+echo "Ping is enabled for this session" && sleep 2
+
+read -p "Do you wish to enable ipv4 ping for your use by default? (y/n) " yn
+
+case $yn in 
+	y ) 
+        gid=$(id -g);
+        ping_allow_write=net.ipv4.ping_group_range="0 $gid";
+        sudo echo "$ping_allow_write;" > /etc/sysctl.d/local.conf;
+        echo "The process is finished. Closing in 3 seconds" && sleep 1;
+        echo "exiting." && sleep 1;
+        echo "exiting.." && sleep 1;
+        echo "exiting..." && sleep 1;
+        kill $PPID;;
+	
+    n ) 
+        echo Closing in 3 seconds && sleep 1;
+        echo "exiting." && sleep 1;
+        echo "exiting.." && sleep 1;
+        echo "exiting..." && sleep 1;
+		kill $PPID;;
+esac
