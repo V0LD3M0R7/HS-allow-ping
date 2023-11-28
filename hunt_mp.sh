8498bede6e2af45ca5fba0caaaf9febecc108166
Vít Peukert <110905408+V0LD3M0R7@users.noreply.github.com>
2023-11-28 14:02:05 +0100

    Update hunt_mp.sh

--- hunt_mp.sh
+++ hunt_mp.sh
@@ -5,23 +5,27 @@ sudo sysctl -w net.ipv4.ping_group_range="0 2147483647"
 echo " "
 echo "Ping is enabled for this session" && sleep 2
 
-read -p "Do you wish to enable ipv4 ping for your use by default? (y/n) " yn
+read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n) " yn
 
-case $yn in 
-	y ) 
-        gid=$(id -g);
-        ping_allow_write=net.ipv4.ping_group_range="0 $gid";
-        sudo echo "$ping_allow_write;" > /etc/sysctl.d/local.conf;
-        echo "The process is finished. Closing in 3 seconds" && sleep 1;
-        echo "exiting." && sleep 1;
-        echo "exiting.." && sleep 1;
-        echo "exiting..." && sleep 1;
-        kill $PPID;;
+# not sure if `while true; do` is neccesery
+while true; do
+    case $yn in 
+	    [Yy]* ) 
+            gid=$(id -g);
+            ping_allow_write=net.ipv4.ping_group_range="0 $gid";
+            sudo echo "$ping_allow_write;" > /etc/sysctl.d/local.conf;
+            echo "The process is finished. Closing in 3 seconds" && sleep 1;
+            echo "closing." && sleep 1;
+            echo "closing.." && sleep 1;
+            echo "closing..." && sleep 1;
+            kill $PPID;;
 	
-    n ) 
-        echo Closing in 3 seconds && sleep 1;
-        echo "exiting." && sleep 1;
-        echo "exiting.." && sleep 1;
-        echo "exiting..." && sleep 1;
-		kill $PPID;;
-esac
+        [Nn]* ) 
+            echo "Closing in 3 seconds" && sleep 1;
+            echo "closing." && sleep 1;
+            echo "closing.." && sleep 1;
+            echo "closing..." && sleep 1;
+		    kill $PPID;;
+        * )
+            echo "Please answer yes or no.";;
+    esac
