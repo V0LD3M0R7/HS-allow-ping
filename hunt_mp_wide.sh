2c17a35cc240396ceccce89f421b0d4a00a66e90
V0LD3M0R7 <110905408+V0LD3M0R7@users.noreply.github.com>
2024-07-31 16:33:33 +0200

    Removed empty echo and inserted \n

--- hunt_mp_wide.sh
+++ hunt_mp_wide.sh
@@ -4,18 +4,15 @@ echo "The script will now enable ipv4 ping for current session" && sleep 2
 echo
 sudo sysctl -w net.ipv4.ping_group_range="0 2147483647"
 echo
-echo "Ping is enabled for this session" && sleep 2
-echo
+echo "Ping is enabled for this session \n" && sleep 2
 read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n) " yn
 
-# not sure if `while true; do` is neccesery
 while true; do
     case $yn in
 
 	[Yy]* )
 		echo 'net.ipv4.ping_group_range="0 2147483647"' ´| sudo tee /etc/sysctl.d/local.conf;
-		echo "The process is finished." && sleep 1;
-		echo ;
+		echo "The process is finished. \n" && sleep 1;
 		read -rsn1 -p"Press enter to close";
 		kill $PPID;;
 
