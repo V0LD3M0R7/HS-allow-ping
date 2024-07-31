3ce71cb0a82d41db907bcd029ffa869d286dc3dd
V0LD3M0R7 <peukert.vit@gmail.com>
2024-07-31 13:35:03 +0200

    Fixed the functionality of scripts
    - removed non-needed sleep lines in n) answers
    - fixed looping for invalid response by using enter prompt and asking
    for re-runing the script
    - some basic clean-up of the code

--- hunt_mp.sh
+++ hunt_mp.sh
@@ -1,34 +1,35 @@
 #! /bin/bash
 
-echo "The script will now enable ipv4 ping for current session" && sleep 2
+echo "The script will now enable EAC ipv4 ping for current session" && sleep 2
 echo
 gid=$(id -g)
 sudo sysctl -w net.ipv4.ping_group_range="0 $gid"
 echo
-echo "Ping is enabled for this session" && sleep 2
+echo "EAC ping is enabled for this session" && sleep 2
 echo
 read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n) " yn
 
-# not sure if `while true; do` is neccesery
 while true; do
     case $yn in
 
-	    [Yy]* ) 
-            ping_allow_write=net.ipv4.ping_group_range="0 $gid";
-            sudo echo "$ping_allow_write;" > /etc/sysctl.d/local.conf;
-            echo "The process is finished. Closing in 3 seconds" && sleep 1;
-            echo ;
-            read -rsn1 -p"Press enter to close";
-            kill $PPID;;
+	[Yy]* )
+		ping_allow_write=net.ipv4.ping_group_range="0 $gid";
+		echo "$ping_allow_write;" | sudo tee /etc/sysctl.d/local.conf;
+		echo "The process is finished. Closing in 3 seconds" && sleep 1;
+		echo ;
+		read -rsn1 -p"Press enter to close";
+		kill $PPID;;
 
-        [Nn]* ) 
-            echo "Closing in 3 seconds" && sleep 1;
-            echo ;
-            read -rsn1 -p"Press enter to close";
-		    kill $PPID;;
+	[Nn]* )
+                echo "The process is finished. \n"
+		read -rsn1 -p"Press enter to close";
+		kill $PPID;;
 
-        * )
-            echo "Please answer yes or no.";;
-
+# all other answers
+	* )
+		echo "Please answer yes or no.";
+		echo "invalid response - please execute the script again \n";
+		read -rsn1 -p"Press enter to close";
+		kill $PPID;;
     esac
 done
--- hunt_mp_wide.sh
+++ hunt_mp_wide.sh
@@ -12,20 +12,23 @@ read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n)
 while true; do
     case $yn in
 
-	    [Yy]* ) 
-            sudo echo 'net.ipv4.ping_group_range="0 2147483647"' > /etc/sysctl.d/local.conf;
-            echo "The process is finished." && sleep 1;
-            echo ;
-            read -rsn1 -p"Press enter to close";
-            kill $PPID;;
+	[Yy]* )
+		echo 'net.ipv4.ping_group_range="0 2147483647"' ´| sudo tee /etc/sysctl.d/local.conf;
+		echo "The process is finished." && sleep 1;
+		echo ;
+		read -rsn1 -p"Press enter to close";
+		kill $PPID;;
 
-        [Nn]* ) 
-            echo "Closing in 3 seconds" && sleep 1;
-            echo ;
-            read -rsn1 -p"Press enter to close";
-            kill $PPID;;
-        * )
-            echo "Please answer yes or no.";;
+	[Nn]* )
+		echo "The process is finished. \n"
+		read -rsn1 -p"Press enter to close";
+		kill $PPID;;
 
+# all other answers
+	* )
+		echo "Please answer yes or no.";
+		echo "invalid response - please execute the script again \n";
+		read -rsn1 -p"Press enter to close";
+		kill $PPID;;
     esac
 done
