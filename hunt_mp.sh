d856ceef886aa706c7621e7177a38953212b1786
V0LD3M0R7 <peukert.vit@gmail.com>
2024-12-05 11:25:13 +0100

    Script revision using ChatGPT
    Fixed many of my first-timer mistakes

--- hunt_mp.sh
+++ hunt_mp.sh
@@ -1,33 +1,45 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "The script will now enable EAC ipv4 ping for current session" && sleep 2
+echo "The script will now enable EAC IPv4 ping for the current session..." && sleep 2
 echo
-gid=$(id -g)
-sudo sysctl -w net.ipv4.ping_group_range="0 $gid"
-echo
-echo "EAC ping is enabled for this session" && sleep 2
-echo
-read -p "Do you wish to enable ipv4 ping for your user account by default? (y/n) " yn
 
+# Get the group ID
+group_id=$(id -g)
+if [[ -z "$group_id" ]]; then
+    echo "Error: Unable to fetch group ID." >&2
+    exit 1
+fi
+
+# Apply the setting temporarily
+sudo sysctl -w net.ipv4.ping_group_range="$group_id $group_id"
+echo
+echo "EAC ping is enabled for this session." && sleep 2
+echo
+
+# Prompt the user
 while true; do
+    read -p "Do you wish to enable IPv4 ping for your user account by default? (y/n): " yn
     case $yn in
-
-	[Yy]* )
-		ping_allow_write=net.ipv4.ping_group_range="0 $gid";
-		echo "$ping_allow_write;" | sudo tee /etc/sysctl.d/local.conf;
-		echo "The process is finished. \n" && sleep 1;		read -rsn1 -p"Press enter to close";
-		kill $PPID;;
-
-	[Nn]* )
-                echo "The process is finished. \n"
-		read -rsn1 -p"Press enter to close";
-		kill $PPID;;
-
-# all other answers
-	* )
-		echo "Please answer yes or no.";
-		echo "invalid response - please execute the script again \n";
-		read -rsn1 -p"Press enter to close";
-		kill $PPID;;
+        [Yy]* )
+            config_line="net.ipv4.ping_group_range=$group_id $group_id"
+            echo "$config_line" | sudo tee /etc/sysctl.d/local.conf >/dev/null
+            echo
+            echo "Configuration has been updated successfully."
+            echo "Run 'sudo sysctl --system' to apply the changes permanently."
+            break
+            ;;
+        [Nn]* )
+            echo
+            echo "No changes were made to the user configuration."
+            break
+            ;;
+        * )
+            echo "Invalid response. Please answer yes (y) or no (n)."
+            ;;
     esac
 done
+
+echo
+read -rsn1 -p "Press any key to close."
+echo
+exit 0
